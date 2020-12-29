6d930d0a2728d9fdc1cba77d9447042b997a01d1
Cristin O'Connor <her@cristin.io>
2020-12-29 00:06:22 -0700

    Made theme more mobile friendly

--- components/Card.js
+++ components/Card.js
@@ -103,26 +103,33 @@ const Card = async (username, theme = false) => {
     }, 0);
 
     const style = `<style>
-      #github-stats-card {max-width: 450px; padding: 0; background-color: #fff; box-sizing: border-box; font-family: Arial, sans-serif;  display: flex; border: 1px solid #efefef; flex-direction: column;}
+      #github-stats-card * {box-sizing: border-box;}
+      #github-stats-card {max-width: 320px; margin: 0; width: 100%; padding: 0; background-color: #fff; box-sizing: border-box; font-family: Arial, sans-serif;  display: flex; border: 1px solid #efefef; flex-direction: column; flex-basis: 100%; font-size: 12px;}
       
       #github-stats-card header h3 {display: flex; justify-content: center; align-items: center;}
       #github-stats-card header h3 img,
       #github-stats-card header h3 a { display: flex; flex-direction: column; }
-      #github-stats-card header h3 a { display: inline-block; text-decoration: none; color: #212121; max-width: calc(100% - 50px); }
-      #github-stats-card header img { height: 30px; margin-left: 10px; }
+      #github-stats-card header h3 a { display: inline-block; text-decoration: none; color: #212121; max-width: calc(100% - 30px); }
+      #github-stats-card header img { width: 20px; margin-left: 10px; }
       
-      #github-stats-card .subheader { display: flex; justify-content: center; padding: 10px 15px; background-color: #efefef;}
-      #github-stats-card ol, ul {display: flex; flex-direction: column; align-items: center; margin: 0; padding: 0 0 10px 10px;}
+      #github-stats-card .subheader { display: flex; justify-content: center; max-width: 100%; padding: 10px 15px; background-color: #efefef;}
+      #github-stats-card ol, ul {max-width: 100%; display: flex; flex-direction: column; align-items: center; margin: 0; padding: 0 0 10px 10px;}
       #github-stats-card li { margin: 5px 0; }
       #github-stats-card li small { display: block; }
-      #github-stats-card .top {width: 100%; padding-bottom: 10px; border-bottom: 1px solid #efefef; margin-bottom: 10px; text-align: center; flex-direction: row; justify-content: space-around;}
-      #github-stats-card .top > li {display: flex; flex-direction: column;}
+      #github-stats-card p {width: 100%; text-align: center;}
+      #github-stats-card .top {display: flex; width: 100%; flex-direction: row; padding-bottom: 10px; border-bottom: 1px solid #efefef; margin-bottom: 10px; text-align: center; justify-content: space-around;}
+      
       #github-stats-card .bottom { list-style: none; }
       
-      #github-stats-card .content {display: flex; justify-content: center; flex-wrap: wrap; font-size: 16px; padding: 15px;}
+      #github-stats-card .content {display: flex; justify-content: center; flex-wrap: wrap; padding: 15px; }
+
+      #github-stats-card .content,
+      #github-stats-card .top,
+      #github-stats-card .bottom,
+      #github-stats-card header { max-width: 100%; }
     </style>`;
 
-    let element = `<div id="github-stats-card" class='container'><header><h3><a href="${user.html_url}" target="_blank" rel="noopener nofollow">@${username} <i>on</i> GitHub</a><img src="https://github.githubassets.com/images/icons/emoji/octocat.png?v8" alt="Octocat"/></h3><div class='subheader'><b>${user.public_repos}</b>&nbsp;Public Repos&nbsp;|&nbsp;<b>${user.public_gists}</b>&nbsp;Public Gists</div></header>
+    let element = `<div id="github-stats-card"><header><h3><a href="${user.html_url}" target="_blank" rel="noopener nofollow">@${username} <i>on</i> GitHub</a><img src="https://github.githubassets.com/images/icons/emoji/octocat.png?v8" alt="Octocat"/></h3><div class='subheader'><b>${user.public_repos}</b>&nbsp;Public Repos&nbsp;|&nbsp;<b>${user.public_gists}</b>&nbsp;Public Gists</div></header>
     <div class='content'><p><b>Top Languages</b></p><ol class='top'><li><em>${topLanguages[1][0]}</em><small>${topLanguages[1][1]} repo(s)</small></li><li><em>${topLanguages[0][0]}</em><small>${topLanguages[0][1]} repo(s)</small></li><li><em>${topLanguages[2][0]}</em><small>${topLanguages[2][1]} repo(s)</small></li></ol><ul class='bottom'></li><li><b>Total Watchers</b>: ${watchers}</li><li><b>All Open Issues</b>: ${openIssues}</li><li><b>Stargazers</b>: ${stargazers}</li></ul></div></div>`;
     
     if (theme) {
